fbe5fbc6479c28d8ef95074740372ba85149c5a8
zhangxin <zhangxin19870504@163.com>
2021-01-31 22:24:02 +0800

    demo_zx2.py检测不到车的不保存图片

--- demo/demo_zx2.py
+++ demo/demo_zx2.py
@@ -189,9 +189,9 @@ if __name__ == "__main__":
                                                 2))
         else:
             # cv2.imwrite(img_save_path, img)
-            with open(path, 'rb') as file_in:
-                with open(img_save_path, 'wb') as file_out:
-                    file_out.write(file_in.read())
+            # with open(path, 'rb') as file_in:
+            #     with open(img_save_path, 'wb') as file_out:
+            #         file_out.write(file_in.read())
             h, w = img.shape[:2]
             fo_nocar.write("{}\t{}\t{}\t{}\n".format(os.path.join(sub_dir, filename),
                                                 ",".join([str(x) for x in [0, 0, w, h]]),
